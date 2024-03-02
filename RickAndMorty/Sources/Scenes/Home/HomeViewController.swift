68577b4e70804862776e96167f6e919adca4b443
Duda Porto <dudabatistaporto@gmail.com>
2024-03-02 02:46:10 -0300

    setup: add assets and swiftgen

--- RickAndMorty/Sources/Scenes/Home/HomeViewController.swift
+++ RickAndMorty/Sources/Scenes/Home/HomeViewController.swift
@@ -3,12 +3,21 @@ import UIKit
 final class HomeViewController: UIViewController {
     private let useCase = CharacterListUseCase()
     
+    private lazy var label = UILabel.build(type: .largeTitle, text: "LargeTitle", color: Color.gray3.color)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(label)
+        view.backgroundColor = .white
+                
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
         useCase.fetchCharacters { [weak self] response in
             print(response.characters)
-            self?.view.backgroundColor = .green
+            self?.label.text = response.characters.first?.name
         } failure: { [weak self] in
             print("ERRO")
             self?.view.backgroundColor = .red
---
+++ SwiftGen.sh
@@ -0,0 +1,7 @@
+BASE_PATH="RickAndMorty/Sources/Generated"
+
+mkdir -p "$BASE_PATH"
+touch "${BASE_PATH}/ColorSet.swift"
+touch "${BASE_PATH}/ImageSet.swift"
+
+swiftgen
